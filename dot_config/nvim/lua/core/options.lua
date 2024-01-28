054e263d2f93d213c948882f20f606568a2dac2e
Jonas Krüger Svensson <jonas.svensson@intility.no>
2024-01-28 15:36:44 +0100

    backup

--- dot_config/nvim/lua/core/options.lua
+++ dot_config/nvim/lua/core/options.lua
@@ -1,61 +1,46 @@
 local o = vim.opt
-local g = vim.g
 
-o.guicursor = ""
-o.mouse = "a"
-o.cursorline = true
-o.completeopt = { "menuone", "noselect" }
-o.clipboard = "unnamedplus"
-o.fileencoding = "utf-8"
+-- Inspired by 
+-- Terje https://github.com/TerjeLafton/Neovim/blob/main/lua/core/options.lua
+-- ThePrimagen https://github.com/ThePrimeagen/init.lua/blob/master/lua/theprimeagen/set.lua
+
+-- General
+o.autowrite = true -- Autowrite file on lost focus
+o.clipboard = "unnamedplus" -- Use system clipboard
+o.cmdheight = 1 -- The height of the bottom bar
+o.confirm = true -- Ask for confirmation when exiting a file with unsaved changes
+o.cursorline = true -- Have the cursors current line be shown in a different color
+o.hlsearch = false -- Do not highlight all search results
+o.incsearch = true -- Incremental search
+o.mouse = "a" -- Enable mouse mode
+o.signcolumn = "yes" -- Always show the signcolumn
+o.updatetime = 50  -- Fast update time
+o.colorcolumn = "120"  -- Column at char 120
+o.showmode = false -- Don't show mode in the cmdline
+o.spelllang = { "en" }
+o.termguicolors = true -- True color support
+o.updatetime = 300 -- Set cursorhold
+o.wildmode = "longest:full,full" -- Command-line completion mode
+o.wrap = false -- Disable line wrap
+
+-- Line numbers
 o.number = true
 o.relativenumber = true
-o.fillchars = { eob = " " }
-o.tabstop = 4
-o.softtabstop = 4
-o.shiftwidth = 4
-o.expandtab = true
-o.hlsearch = false
-o.incsearch = true
-o.copyindent = true
-o.smartindent = true
-o.wrap = false
-o.history = 100
-o.ignorecase = true
-o.laststatus = 3
-o.lazyredraw = true
-o.preserveindent = true
-o.pumheight = 10
-o.scrolloff = 8
-o.signcolumn = "yes"
-o.smartcase = true
-o.splitbelow = true
-o.splitright = true
-o.swapfile = false
-o.termguicolors = true
-o.timeoutlen = 300
-o.undofile = true
-o.updatetime = 300
-o.writebackup = false
 
-g.mapleader = " "
-g.do_filetype_lua = 1
-g.did_load_filetypes = 0
-g.highlighturl_enabled = true
-g.zipPlugin = false
-g.load_black = false
-g.loaded_2html_plugin = true
-g.loaded_getscript = true
-g.loaded_getscriptPlugin = true
-g.loaded_gzip = true
-g.loaded_logipat = true
-g.loaded_matchit = true
-g.loaded_netrwFileHandlers = true
-g.loaded_netrwPlugin = true
-g.loaded_netrwSettings = true
-g.loaded_remote_plugins = true
-g.loaded_tar = true
-g.loaded_tarPlugin = true
-g.loaded_zip = true
-g.loaded_zipPlugin = true
-g.loaded_vimball = true
-g.loaded_vimballPlugin = true
+-- Smart tabs
+o.smartindent = true -- Insert indents automatically
+o.tabstop = 4  -- Number of spaces tabs count for
+o.softtabstop = 4 -- Indent size
+o.shiftwidth = 4  -- Indent size
+o.expandtab = true -- Tabs into spaces
+o.shiftround = true -- Round indent
+o.number = true -- Print line numbers
+o.scrolloff = 8 -- Lines of context on the end of the buffer
+
+-- backup is handled by undotree
+-- oooor telescope-undo.nvim...? I'll have to investigate
+--o.swapfile = false
+--o.backup = false
+--o.undodir = os.getenv("HOME") .. "/.vim/undodir"
+--o.undofile = true
+
